55665e98fd55cf6a580ce8a00f200e693e78c62b
arukh89 <85455057+arukh89@users.noreply.github.com>
2025-09-29 07:41:26 +0700

    Update global.d.ts

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,7 +1,9 @@
+// types/global.d.ts
 export {};
 
 declare global {
   interface Window {
     APP_NAME?: string;
+    sdk?: any; // minimal supaya TS tidak komplain; bisa diganti dengan tipe yang lebih spesifik
   }
 }
